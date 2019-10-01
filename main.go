63e904196fe66e23cf7f268afaa4ef98eb091160
onda-koyo <onda-koyo@dmm.com>
2019-10-01 22:36:50 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
 
 func main() {
-	fmt.Println("Hello Golang")
+	url := ""
+	run(url, false)
+	run(url, true)
+}
+
+func run(url string, fast bool) {
+	st := time.Now()
+	for i := 0; i < 100; i++ {
+		switch fast {
+		case true:
+			fastHttp(url)
+		case false:
+			normalHttp(url)
+		}
+	}
+	ed := time.Now()
+	fmt.Println()
+	fmt.Println(ed.Sub(st))
+}
+
+func fastHttp(url string) {
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+
+	defer fasthttp.ReleaseRequest(req)   // <- do not forget to release
+	defer fasthttp.ReleaseResponse(resp) // <- do not forget to release
+
+	req.Header.SetMethod("GET")
+	req.SetRequestURI(url)
+
+	fasthttp.Do(req, resp)
+
+	bodyBytes := resp.Body()
+	operation(bodyBytes)
+}
+
+func normalHttp(url string) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr, Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	operation(body)
+}
+
+func operation(body []byte) {
+	fmt.Print(len(body), ",")
 }
