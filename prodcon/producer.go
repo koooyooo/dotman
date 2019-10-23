3e2f43234830a1628fb74e362a4c658060a27bb1
koooyooo <jab.cross.hook@gmail.com>
2019-10-24 02:12:11 +0900

    add cond

--- prodcon/producer.go
+++ prodcon/producer.go
@@ -5,20 +5,26 @@ import (
 	"time"
 )
 
-type WorkProducer func(int, int, chan<- struct{}, *sync.WaitGroup)
+type WorkProducer func(int, int, chan<- struct{}, *sync.Cond, *sync.WaitGroup)
 
-func AtOnceProducer(reqPerSec, _ int, workStream chan<- struct{}, wg *sync.WaitGroup) {
+func AtOnceProducer(reqPerSec, _ int, workStream chan<- struct{}, cond *sync.Cond, wg *sync.WaitGroup) {
 	wg.Add(reqPerSec)
 	for j := 0; j < reqPerSec; j++ {
+		cond.L.Lock()
 		workStream <- struct{}{}
+		cond.L.Unlock()
+		cond.Broadcast()
 	}
 }
 
-func PerSecProducer(reqPerSec, sec int, workStream chan<- struct{}, wg *sync.WaitGroup) {
+func PerSecProducer(reqPerSec, sec int, workStream chan<- struct{}, cond *sync.Cond, wg *sync.WaitGroup) {
 	wg.Add(reqPerSec * sec)
 	for i := 0; i < sec; i++ {
 		for j := 0; j < reqPerSec; j++ {
+			cond.L.Lock()
 			workStream <- struct{}{}
+			cond.L.Unlock()
+			cond.Broadcast()
 			time.Sleep(time.Duration(1000/reqPerSec) * time.Millisecond)
 		}
 		//fmt.Print(" ")
