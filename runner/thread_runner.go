66f6fdca858fded7803f2376ca84b0caf6c1c34f
onda-koyo <onda-koyo@dmm.com>
2019-11-07 17:06:32 +0900

    add wait group

--- runner/thread_runner.go
+++ runner/thread_runner.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"sync"
 	"time"
 
 	"github.com/koooyooo/dotman/model"
@@ -11,13 +12,19 @@ func RunThread(isAtOnceMode bool, req model.Request, reqPerSec, sec, numWorkers 
 	var client prodcon.ClientFunc
 	client = prodcon.FastHttp
 
+	var wg = sync.WaitGroup{}
+	wg.Add(reqPerSec * sec)
 	for s := 0; s < sec; s++ {
 		st := time.Now()
 		for r := 0; r < reqPerSec; r++ {
-			go client(req, config)
+			go func() {
+				client(req, config)
+				wg.Done()
+			}()
 		}
 		ed := time.Now()
 		invokeSec := ed.Sub(st).Milliseconds()
 		time.Sleep(time.Duration(1000-invokeSec) * time.Millisecond)
 	}
+	wg.Wait()
 }
